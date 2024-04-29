90355d2b97b196bfbab152adc9ac403d955ea35d
Kamwele-hub <stephen.kamwele@student.moringaschool.com>
2024-04-29 18:17:58 +0300

    distance and city name complete

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+ 
+  if (distanceInFeet <= 400){
+    return "This one is on me!"
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500){
+    return "I will gladly take your thirty bucks."
+  } else if (distanceInFeet > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  return cityName === "NYC" ? "Ok, sounds good." : "No go."
 }
 
 function switchOnCharmFromTip(){
